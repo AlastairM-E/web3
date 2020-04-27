ff9ef6ab9f1c8ef15758724e7a40263f079bd669
AlastairM-E <alastairunityemail@gmail.com>
2020-04-27 22:05:34 +0100

    completed stage one of the MobileMenu, there will be more TDD and features to add in later on

--- src/components/MobileMenu/MobileMenu.test.tsx
+++ src/components/MobileMenu/MobileMenu.test.tsx
@@ -4,7 +4,14 @@ import { render, fireEvent } from '@testing-library/react';
 import MobileMenu from './MobileMenu';
 
 /* TESTS */
-test('Check that the MobileMenu makes a SideMenu', () => {
+test('Check that the MobileMenu makes a SideMenu only', () => {
+  // get container --> check only the MobileMenu component is inside.
+  // Also check this by the getByTestId('MobileMenu') method.
+  // check that the SideMenu does not exist --> therefore the selectory returns undefined.
+  // Click on the MobileMenu Component --> this should create a SideMenu.
+  // check that when the SideMenu is queried for, it returns a truthy value.
+  // Click on the MobileMenu Component again --> should remove the SideMenu Component.
+  // When the SideMenu is queried for again, should return nothing.
   const { container, getByTestId } = render(<MobileMenu>|||</MobileMenu>);
   expect(container.children.length).toBe(1);
 
@@ -12,14 +19,42 @@ test('Check that the MobileMenu makes a SideMenu', () => {
   expect(doesSideMenuExist()).toBeUndefined();
 
   const virtualMobileMenu = getByTestId('MobileMenu');
-
-  fireEvent(
-    virtualMobileMenu,
-    new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-    }),
-  );
+  fireEvent.click(virtualMobileMenu);
 
   expect(doesSideMenuExist()).toBeTruthy();
+
+  fireEvent.click(virtualMobileMenu);
+
+  expect(doesSideMenuExist()).toBeUndefined();
+});
+
+
+test('The MobileMenu should return a SideMenu which has a button which will close the SideMenu', () => {
+  // Render(<MobileMenu>|||</MobileMenu>) = { getBYTestId };
+  // Have Variable on the queries for the data-testid to = sideMenu and also CloseSideMenuButton.
+  // Check whetehr these value (which are functions) resolve true. They should return falsey.
+  // Then the menu should be clicked.
+  // The CloseSideMenuButton and SideMenu should return truthy.
+  // Then the CloseSideMenuButton should be clicked.
+  // This should mean that the ClsoeSideMenutButton and the SideMenu should both not exist.
+  // Therefore, the should before return undefined.
+  const { getByTestId } = render(<MobileMenu>|||</MobileMenu>);
+
+  const doesSideMenuExist = () => document.querySelectorAll('[data-testid="SideMenu"]')[0];
+  const doesCloseSideMenuButtonExist = () => document.querySelectorAll('[data-testid="CloseSideMenuButton"]')[0];
+
+  expect(doesSideMenuExist()).toBeUndefined();
+  expect(doesCloseSideMenuButtonExist()).toBeUndefined();
+
+  const virtualMobileMenu = getByTestId('MobileMenu');
+  fireEvent.click(virtualMobileMenu);
+
+  expect(doesSideMenuExist()).toBeTruthy();
+  expect(doesCloseSideMenuButtonExist()).toBeTruthy();
+
+  const virtualCloseSideMenuButton = document.querySelectorAll('[data-testid="CloseSideMenuButton"]')[0];
+  fireEvent.click(virtualCloseSideMenuButton);
+
+  expect(doesSideMenuExist()).toBeUndefined();
+  expect(doesCloseSideMenuButtonExist()).toBeUndefined();
 });
--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -3,6 +3,12 @@ import React, { useState, Fragment } from 'react';
 
 import styled from 'styled-components';
 
+/* INTERFACES */
+interface childrenProperty {
+  children: string
+}
+
+/* STYLES */
 const StyledMobileMenu = styled.span`
     display: none;
     padding: 10px 10px 20px 10px;
@@ -18,24 +24,56 @@ const StyledMobileMenu = styled.span`
     }
 `;
 
-const SideMenu = styled.span``;
+const StyledSideMenu = styled.span``;
+const StyledCloseSideMenuButton = styled.button``;
 
 /* COMPONENT */
-function MobileMenu({ children }: { children: string }) {
+function MobileMenu({ children }: childrenProperty) {
+  // Create isMobileMenuClicked & setIsMobileMenuClicked useState (initial state will be false).
+  // Create a toggleSideMenu function is order so that the Side Menu can be toggled off and on.
+  // Create a SideMenu Component that will return a SideMenu with a data-testid of "SideMenu".
+  // Inside that component is a CloseSideMenuButton, with an event listener which will trigger
+  // the toggleSideMenu Function.
+  // The function will render a wrapper Fragment component,in whcih
+  // the StyledMobileMenu component which renders the children prop.
+  // The StyledMobileMenu will have an onclick event handler which when clicked will toggle
+  // the SideMenu.
+  // This occurs becuase the SideMenu component is conditionally rendered
+  // based on whether the  isMobileMenuClicked state is true (by default is is false).
+  // This means that if the Menu Button is click it will open the sideMenu tab and
+  // (currently) the SideMenu and the MobileMenu button will then be able to close
+  // the side menu.
+  /* HOOKS */
   const [isMobileMenuClicked, setIsMobileMenuClicked] = useState(false);
+
+  /* EVENT LISTENERS */
+  const toggleSideMenu = () => setIsMobileMenuClicked(!isMobileMenuClicked);
+
+  /* ADDItIONAL COMPONENTS */
+  function SideMenu() {
+    return (
+      <StyledSideMenu data-testid="SideMenu">
+        <StyledCloseSideMenuButton
+          onClick={toggleSideMenu}
+          data-testid="CloseSideMenuButton"
+        >
+          X
+        </StyledCloseSideMenuButton>
+      </StyledSideMenu>
+    );
+  }
 
   /* RENDER */
   return (
     <>
       <StyledMobileMenu
         data-testid="MobileMenu"
-        onClick={() => setIsMobileMenuClicked(!isMobileMenuClicked)}
+        onClick={toggleSideMenu}
       >
         {children}
       </StyledMobileMenu>
-      {isMobileMenuClicked ? <SideMenu data-testid="SideMenu" /> : null}
+      {isMobileMenuClicked ? <SideMenu /> : null}
     </>
-
   );
 }
 
