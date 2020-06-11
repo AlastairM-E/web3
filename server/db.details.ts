241e2cf8519ce89d1456e1bcf4ac7dbaf261ae1f
AlastairM-E <alastairunityemail@gmail.com>
2020-06-11 20:45:52 +0100

    changes

---
+++ server/db.details.ts
@@ -0,0 +1,11 @@
+export {};
+
+const url = 'mongodb+srv://m001-student:m001-mongodb-basics@mongounicluster-ejta2.mongodb.net/MongoUniCluster';
+const dbName = 'test-db';
+const collectionName = 'test-collection';
+
+module.exports = {
+  url,
+  dbName,
+  collectionName,
+};
---
+++ server/index.ts
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+const { url, dbName, collectionName } = require('./db.details');
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(async (err) => {
+  if (err) throw err;
+
+  const db = client.db(dbName);
+  const testCollection = db.collection(collectionName);
+
+  const findCount = () => testCollection.find({ count: 1, accumulator: 1 });
+  const { cmd } = findCount();
+  const { query } = cmd;
+
+  await testCollection.updateOne(query,
+    {
+      $set: {
+        count: query.count + query.accumulator,
+        accumulator: query.accumulator,
+      },
+    });
+  await client.close();
+});
